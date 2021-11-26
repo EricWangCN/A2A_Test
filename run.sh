775e03cf777ae145a3f05e9c421cb883ffcac4b7
Zilong Wang <wangzilong2016@outlook.com>
2021-11-26 16:29:22 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-NCCL_DEBUG=INFO OMP_NUM_THREADS=1 python -m torch.distributed.launch --nnodes=1 --nproc_per_node=8 main.py
+NCCL_DEBUG=INFO OMP_NUM_THREADS=1 python -m torch.distributed.launch --nnodes=1 --nproc_per_node=8 $1.py
